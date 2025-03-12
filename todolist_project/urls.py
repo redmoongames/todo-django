45f9cb5941b9ddb8eab7608159fe70a85905c10e
redmoongames <redmoon.thebest@gmail.com>
2025-03-12 12:09:25 +0300

    url change

--- todolist_project/urls.py
+++ todolist_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('todo_api.urls')),
+    path('/', include('todo_api.urls')),
 ]
